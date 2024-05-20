a2936dcc4034e14824d5bc805f01089e87490c38
vmonea <vitormonea2000@gmail.com>
2024-05-20 11:20:04 -0300

    att: deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,7 +15,7 @@ import { DevService } from './data/services/dev.service';
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRootAsync({
-      useClass: DevService,
+      useClass: ProdService,
       imports: [ConfigModule],
     }),
     PostagemModule,
